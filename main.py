a28e0eabdeb174ddc3e5367b5bc22a7e85cff6e3
ChembsTacoma <chembiang3@gmail.com>
2024-12-21 15:23:47 -0600

    actual snake code started

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('Sup Gang')
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "green"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+
+
