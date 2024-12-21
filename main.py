0e4652de2b5bda39a0a0f801757c1b9718caf401
ChembsTacoma <chembiang3@gmail.com>
2024-12-21 15:36:58 -0600

    all constant variables made

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "green"
+SNAKE_COLOR = "#6851d6"
+FOOD_COLOR = "#24c74f"
+BACKGROUND_COLOR = "#000000"
+
+
 
 class Snake:
     pass
